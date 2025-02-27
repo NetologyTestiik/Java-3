f1ff7c9e275533c31b0497c131c970dbe1658429
Neto test <veronikakaravanova60@gmail.com>
2025-02-27 16:57:59 +0300

    change README

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int startingScore = 100; // начальный счет
+        int amout = 1200; // сумма пополнения
+
+        int bonus;
+        if (amout > 1001) {
+            bonus = amout / 100;
+        } else {
+            bonus = 0;
+        }
+int totalamout = amout + bonus;
+        System.out.println("Итоговая сумма счета: " + totalamout);
+        System.out.println("Итоговая сумма бонусов: " + bonus);
+    }
+}
